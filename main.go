32669fc9592ff1eb6e0e2ba5fe564a1cad61eb32
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2025-05-29 18:06:30 +0200

    no longer serving img folder

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	http.Handle("/style/", http.StripPrefix("/style/", http.FileServer(http.Dir("./static/style"))))
 	http.Handle("/other/", http.StripPrefix("/other/", http.FileServer(http.Dir("./static/other"))))
 	http.Handle("/pwa/", http.StripPrefix("/pwa/", http.FileServer(http.Dir("./static/pwa"))))
-	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
+	// http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
 
 	// Page routes
 	http.HandleFunc("/", logRequest(router))
